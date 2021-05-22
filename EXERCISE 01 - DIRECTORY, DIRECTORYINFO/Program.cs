442267dc5c2219dc3146a37ae643660f8326f302
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-22 09:38:34 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - DIRECTORY, DIRECTORYINFO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace EXERCISE_01___DIRECTORY__DIRECTORYINFO {
+    //-----> START
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\temp\myfolder";
+
+            try {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, ".", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach(string s in folders) {
+                    Console.WriteLine(s);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
